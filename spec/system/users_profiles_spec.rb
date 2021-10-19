bc89d3db13c80040b461588e4f4018e685aced57
rstar <rstar0823@icloud.com>
2021-10-19 11:56:10 +0900

    Add follow form

--- spec/system/users_profiles_spec.rb
+++ spec/system/users_profiles_spec.rb
@@ -16,12 +16,16 @@ RSpec.describe "UsersProfiles", type: :system do
     expect(page).to have_title full_title(user.name)
     expect(page).to have_selector 'h1', text: user.name
     expect(page).to have_selector 'h1>img.gravatar'
-    expect(page).to have_content Micropost.count 
+    expect(page).to have_content Micropost.count
+    expect(page).to have_content user.following.count
+    expect(page).to have_content user.followers.count
     expect(page).to have_selector 'div.pagination' 
     user.microposts.paginate(page: 1).each do |micropost|
       expect(page).to have_content micropost.content
     end
   end
+
+
 end
     
 
