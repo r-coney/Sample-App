8cd0d4026a717bf4291e372ce0856d3d5ebdd240
rstar <rstar0823@icloud.com>
2021-10-22 20:23:19 +0900

    refactoring user_mailer_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'full_title' do
-    context 'no argument' do
-      it 'return [More Beauty]' do
-        expect(full_title).to eq("More Beauty")
-      end
+  describe '#full_title' do
+    context '引数がない時' do
+      it { expect(full_title).to eq("More Beauty") }
     end
-    context 'argument is "Help"' do
-      it 'retun [Help | More Beauty]' do
-        expect(full_title('Help')).to eq('Help | More Beauty')
-      end
+    context '引数が"Help"の時' do
+      it { expect(full_title('Help')).to eq('Help | More Beauty') }
     end
   end
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,40 +1,46 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "account_activation" do
-    let(:user) { create(:user, :michael) }
+  describe 'account_activation' do
+    let(:user) { create(:user) }
     let(:mail) { UserMailer.account_activation(user) }
-    it "account_activation_preview" do
-      user.activation_token = User.new_token
-      mail = UserMailer.account_activation(user)
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["noreply@example.com"])
+
+    describe 'account_activation_preview' do
+      before do
+        user.activation_token = User.new_token
+        mail = UserMailer.account_activation(user)
+      end
+
+      it { expect(mail.subject).to eq("Account activation") }
+      it { expect(mail.to).to eq([user.email]) }
+      it {expect(mail.from).to eq(["noreply@example.com"]) }
     end
 
-    it "renders the body" do
-      expect(mail.body.encoded).to match user.name
-      expect(mail.body.encoded).to match user.activation_token
-      expect(mail.body.encoded).to match CGI.escape(user.email)
+    describe 'renders the body' do
+      subject { mail.body.encoded }
+
+      it { is_expected.to match user.name }
+      it { is_expected.to match user.activation_token }
+      it { is_expected.to match CGI.escape(user.email) }
     end
   end
   
-  describe "password_reset" do
+  describe 'password_reset' do
     let(:mail) { UserMailer.password_reset(user) }
-    let(:user) { create(:user, :michael) }
+    let(:user) { create(:user) }
+    before { user.reset_token = User.new_token }
     
-    it "renders the headers" do
-      user.reset_token = User.new_token
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq ([user.email])
-      expect(mail.from).to eq(["noreply@example.com"])
+    describe 'renders the headers' do
+      it { expect(mail.subject).to eq("Password reset") }
+      it { expect(mail.to).to eq ([user.email]) }
+      it { expect(mail.from).to eq(["noreply@example.com"]) }
     end
     
-    it "renders the body" do
-      user.reset_token = User.new_token
-      expect(mail.body.encoded).to match user.reset_token
-      expect(mail.body.encoded).to match CGI.escape(user.email)
-      
+    describe 'renders the body' do
+      subject { mail.body.encoded }
+    
+      it { is_expected.to match user.reset_token }
+      it { is_expected.to match CGI.escape(user.email) }
     end
   end
 end
