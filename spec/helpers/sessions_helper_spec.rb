241d1a4cdb9002ddfc3e124d575e6fef1672c657
rstar <rstar0823@icloud.com>
2021-10-22 20:01:16 +0900

    refactoring sessions_helper_spec

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,29 +1,17 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionsHelper. For example:
-#
-# describe SessionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe SessionsHelper, type: :helper do
   describe 'persistent session test' do
-    before(:each) do
-      @user = create(:user)
-      remember(@user)
-    end
+    let(:user) { create(:user) }
+    before { remember(user) }
     
-    it 'current_user returns right user when session is nil' do
-      expect(@user).to eq current_user
+    it 'current_userはセッションがnil場合、正しいユーザーを返す' do
+      expect(user).to eq current_user
       expect(is_logged_in?).to be_truthy
     end
 
-    it 'current_user returns nil when remember digest is wrong' do
-      @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    it 'remember_digestが一致しない場合、nilを返す' do
+      user.update_attribute(:remember_digest, User.digest(User.new_token))
       expect(current_user.nil?).to be_truthy
     end
   end
