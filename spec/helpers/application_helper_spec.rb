edb9cf9b62ef1da3f7375647c45cefa5ba5ac00a
rstar <rstar0823@icloud.com>
2021-10-22 20:58:57 +0900

    refactoring micropost model spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ApplicationHelper, type: :helper do
       it { expect(full_title).to eq("More Beauty") }
     end
     context '引数が"Help"の時' do
-      it { expect(full_title('Help')).to eq('Help | More Beauty') }
+      it { expect(full_title('Help')).to eq("Help | More Beauty") }
     end
   end
 end
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Micropost, type: :model do
-  let(:micropost) { FactoryBot.create(:micropost) }
+  let(:micropost) { create(:micropost) }
 
-  it 'should be valid' do
+  describe 'micropost validation' do
+    
+    it 'micropostが有効' do
     expect(micropost.valid?).to be_truthy
-  end
+    end
   
-  it 'user id should be present' do
-    micropost.user_id = nil
-    expect(micropost.valid?).not_to be_truthy
-  end
+    it 'user_idがnilの場合、有効ではなくなる' do
+      micropost.user_id = nil
+      expect(micropost.valid?).not_to be_truthy
+    end
   
-  it 'content should be present' do
-    micropost.content = ' '
-    expect(micropost.valid?).not_to be_truthy
-  end
+    it 'contentが空文字の場合、有効ではなくなる' do
+      micropost.content = ' '
+      expect(micropost.valid?).not_to be_truthy
+    end
   
-  it 'content shoud be at most 140 characters' do
-    micropost.content = "a" * 141
-    expect(micropost.valid?).not_to be_truthy
+    it 'contentが140文字を超えると、有効ではなくなる' do
+      micropost.content = "a" * 141
+      expect(micropost.valid?).not_to be_truthy
+    end
   end
   
   describe "Sort by latest" do
-      let!(:micropost) { create(:micropost) }
-      let!(:day_before_yesterday) { create(:micropost, :day_before_yesterday, user_id: micropost.user_id) }
-      let!(:now) { create(:micropost, :now, user_id: micropost.user_id) }
-      let!(:yesterday) { create(:micropost, :yesterday, user_id: micropost.user_id) }
+      # let!(:micropost) { create(:micropost) }
+      let!(:day_before_yesterday) { create(:micropost, :day_before_yesterday) }
+      let!(:now) { create(:micropost, :now) }
+      let!(:yesterday) { create(:micropost, :yesterday) }
       
-    it "succeeds" do
-      expect(Micropost.first).to eq now
-    end
+    it { expect(Micropost.first).to eq now }
   end
 end
