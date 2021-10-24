da80f8d44e0afb7d66c7f0cfd818b6faff52cc34
rstar <rstar0823@icloud.com>
2021-10-24 21:46:54 +0900

    refactoring relationships reqest spec

--- spec/requests/relationships_spec.rb
+++ spec/requests/relationships_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Relationships", type: :request do
-  describe "Relationships#create" do
-    it "create should require logged-in user" do
+RSpec.describe 'Relationships', type: :request do
+  describe '#create' do
+    it 'ログインしていなければ、#createに失敗する' do
       expect{ post relationships_path }.to change(Relationship, :count ).by(0)
       expect(response).to redirect_to login_url
     end
   end
 
-  describe "Relationships#destroy" do
+  describe '#destroy' do
   let(:user) { create(:user) }
   let(:other_user) { create(:user) }
-    it 'destroy should require logged-in user' do
+    it 'ログインしていなければ、#destroyに失敗する' do
       expect{ delete relationship_path(other_user) }.to change(Relationship, :count).by(0)
       expect(response).to redirect_to login_url
     end 
