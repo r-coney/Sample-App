59b9f0e1d8848d8d3c7fdca3e51f7e100d66da33
rstar <rstar0823@icloud.com>
2021-10-25 13:20:51 +0900

    refactoring users_profile system spec

--- spec/system/users_profiles_spec.rb
+++ spec/system/users_profiles_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "UsersProfiles", type: :system do
-  let(:user) { FactoryBot.create(:user) }
-  let(:micropost) { FactoryBot.create(:micropost, user_id: user.id) }
+  let(:user) { create(:user) }
+  let(:micropost) { create(:micropost, user_id: user.id) }
 
   before do
     34.times do
@@ -11,21 +11,21 @@ RSpec.describe "UsersProfiles", type: :system do
     end
   end
 
-  it 'profile display' do
+  subject { page }
+
+  it 'profileのdisplayを確認' do
     visit user_path(user)
-    expect(page).to have_title full_title(user.name)
-    expect(page).to have_selector 'h1', text: user.name
-    expect(page).to have_selector 'h1>img.gravatar'
-    expect(page).to have_content Micropost.count
-    expect(page).to have_content user.following.count
-    expect(page).to have_content user.followers.count
-    expect(page).to have_selector 'div.pagination' 
+    is_expected.to have_title full_title(user.name)
+    is_expected.to have_selector 'h1', text: user.name
+    is_expected.to have_selector 'h1>img.gravatar'
+    is_expected.to have_content Micropost.count
+    is_expected.to have_content user.following.count
+    is_expected.to have_content user.followers.count
+    is_expected.to have_selector 'div.pagination' 
     user.microposts.paginate(page: 1).each do |micropost|
       expect(page).to have_content micropost.content
     end
   end
-
-
 end
     
 
