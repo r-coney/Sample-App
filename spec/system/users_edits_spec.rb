c206ac0e7ed96e58a95446d18efd28d8b9607672
rstar <rstar0823@icloud.com>
2021-10-25 12:18:56 +0900

    refactoring users_edits system spec

--- spec/system/users_edits_spec.rb
+++ spec/system/users_edits_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "UsersEdits", type: :system do
-  before do
-    @user = create(:user, :michael)
-  end
+ let(:user) { create(:user) }
 
-  it 'unsuccessful edit' do
-    visit login_path
-    fill_in 'Email', with: 'michael@example.com'
-    fill_in 'Password', with: 'password'
-    click_button "Log in"
-    expect(page).to have_current_path user_path(@user)
-    visit edit_user_path(@user)
+  it '無効な送信の場合、ユーザー情報の編集に失敗する' do
+    signin(user)
+    expect(page).to have_current_path user_path(user)
+    visit edit_user_path(user)
     expect(page).to have_content 'Update your profile'
     fill_in 'Name', with: ''
     fill_in 'Email', with: ''
@@ -21,13 +16,13 @@ RSpec.describe "UsersEdits", type: :system do
     expect(page).to have_content 'Update your profile'
   end
 
-  it 'successful edit friendly forwarding' do
-    visit edit_user_path(@user)
-    fill_in 'Email', with: 'michael@example.com'
-    fill_in 'Password', with: 'password'
+  it 'ユーザー情報の編集とfriendly forwardingに成功する' do
+    visit edit_user_path(user)
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
     click_button "Log in"
-    expect(page).to have_current_path edit_user_path(@user)
-    visit edit_user_path(@user)
+    expect(page).to have_current_path edit_user_path(user)
+    visit edit_user_path(user)
     expect(page).to have_content 'Update your profile'
     fill_in 'Name', with: 'Foo Bar'
     fill_in 'Email', with: 'foo@bar.com'
@@ -36,8 +31,8 @@ RSpec.describe "UsersEdits", type: :system do
     click_button 'Save changes'
     expect(page).to have_content 'Profile updated'
     expect(page).to  have_content 'Foo Bar'
-    @user.reload
-    expect(@user.name).to eq 'Foo Bar' 
-    expect(@user.email).to eq 'foo@bar.com'
+    user.reload
+    expect(user.name).to eq 'Foo Bar' 
+    expect(user.email).to eq 'foo@bar.com'
   end  
 end
