e153fef32cff42ea3d34f335af0e7764320b842f
rstar <rstar0823@icloud.com>
2021-10-25 12:28:53 +0900

    refactoring users_indices system spec

--- spec/system/users_indices_spec.rb
+++ spec/system/users_indices_spec.rb
@@ -1,23 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "UsersIndices", type: :system do
+RSpec.describe 'UsersIndices', type: :system do
+  let!(:users) { create_list(:user, 30) }
+  let(:user) { create(:user) }
 
-include_context "setup"
-
-  before do
-    users
-    @admin = create(:user, :michael)
-    @non_admin = create(:user, :no_admin)
-  end
-
-  it 'index including pagination' do
-    visit login_path
-    fill_in 'Email', with: 'michael@example.com'
-    fill_in 'Password', with: 'password'
-    click_button "Log in"
+  it 'indexページにpaginationリンクが存在している' do
+   signin(user)
     visit users_path
-    expect(page).to have_content 'All users'
-    expect(page).to have_content 'Previous'
+    expect(page).to have_content "All users"
+    expect(page).to have_content "Previous"
     User.paginate(page: 1).each do |user|
       expect(page).to have_css("li", text: user.name)
     end
