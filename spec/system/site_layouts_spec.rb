c96437987d6073d33c4a68df314c3ddb015ace74
rstar <rstar0823@icloud.com>
2021-10-25 11:57:50 +0900

    refactoring site_layouts system spec

--- spec/system/site_layouts_spec.rb
+++ spec/system/site_layouts_spec.rb
@@ -1,39 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'site layout', type: :system do
-  context 'access to root_path' do
+  
+  describe 'root_path' do
     before { visit root_path }
     subject { page }
-    it 'has links sach as root_path, help_path and about_path' do
-      is_expected.to have_link nil, href: root_path
-      is_expected.to have_link 'Help', href: help_path
-      is_expected.to have_link 'About', href: about_path
-      is_expected.to have_link 'Contact', href: contact_path
-      visit contact_path
-      expect(page).to have_title full_title("Contact")
-      visit signup_path
-      expect(page).to have_title full_title("Sign up")
-    end
+      it 'root, help,about,contactのリンクがあることを確認' do
+        is_expected.to have_link nil, href: root_path
+        is_expected.to have_link 'Help', href: help_path
+        is_expected.to have_link 'About', href: about_path
+        is_expected.to have_link 'Contact', href: contact_path
+        visit contact_path
+        is_expected.to have_title full_title("Contact")
+        visit signup_path
+        is_expected.to have_title full_title("Sign up")
+      end
+  end
 
-    before do
-      @user = create(:user,:michael)
-    end
+  describe 'ログインしている時のサイトレイアウト' do
+    let(:user) { create(:user) }
+    subject { page }
 
-    it 'layout links when logged in' do
-      visit login_path
-      fill_in 'Email', with: 'michael@example.com'
-      fill_in 'Password', with: 'password'
-      click_button "Log in"
-      expect(page).to have_current_path user_path(@user)
+    it 'users,profile,settings,logoutのリンクがあることを確認' do
+      signin(user)
+      is_expected.to have_current_path user_path(user)
       visit root_path
-      expect(page).to have_link 'Users', href: users_path
+      is_expected.to have_link 'Users', href: users_path
       click_link 'Account'
-      expect(page).to have_link 'profile', href: user_path(@user)
-      expect(page).to have_link 'settings', href: edit_user_path(@user)
-      expect(page).to have_link 'Log out', href: logout_path
-
+      is_expected.to have_link 'profile', href: user_path(user)
+      is_expected.to have_link 'settings', href: edit_user_path(user)
+      is_expected.to have_link 'Log out', href: logout_path
     end
-
-      
   end
 end
